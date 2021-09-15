30a26db20b4917ba2fcd9e4c43b44ed26eba5d8d
kicbar <bartlomiejkicior@gmail.com>
2021-09-15 13:54:52 +0200

    add Post entity

---
+++ Domain/Entities/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public Post() { }
+
+        public Post(int id, string title, string content)
+        {
+            (Id, Title, Content) = (id, title, content);    
+        }
+    }
+}
