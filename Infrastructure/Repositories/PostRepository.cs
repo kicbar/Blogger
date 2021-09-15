240f8c0dde4a0995677934f5cd073d4868e93f9e
kicbar <bartlomiejkicior@gmail.com>
2021-09-15 14:09:24 +0200

    add PostRepository

---
+++ Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        public IEnumerable<Post> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post Add(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
